8497d3b389aa72afdbb2fd9f32205ca5128bc0eb
Roger Fernandes <roger.osbourne@gmail.com>
2022-05-05 16:03:35 -0300

    test retrieve config arg

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -1,5 +1,14 @@
-def execute():
-    print("Hello World!")
-    
+import argparse
+import json
+
+
+def main():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("-c", "--config", help="Action configs", default="")
+    args = parser.parse_args()
+    cfg = args.config
+    print(cfg)
+
+
 if __name__ == '__main__':
-    execute()
+    main()
