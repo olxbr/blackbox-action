cd786491366f549c4abcd1a978ea125d0638000d
Roger Fernandes <roger.osbourne@gmail.com>
2022-05-05 16:38:15 -0300

    test retrieve config arg

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -7,7 +7,8 @@ def main():
     parser.add_argument("-c", "--config", help="Action configs", default="")
     args = parser.parse_args()
     cfg = args.config
-    print(cfg["test"])
+    j = json.loads(cfg)
+    print(f"O valor de teste é {j['test']}")
 
 
 if __name__ == '__main__':
